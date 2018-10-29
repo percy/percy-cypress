7253b2d073f730ab83760875203a1c4aee26498d
David Jones <dgjones@gmail.com>
2018-10-29 15:55:39 -0700

    adding stronger typing and use const over let

--- lib/environment.ts
+++ lib/environment.ts
@@ -1,8 +1,8 @@
 declare var Cypress: any
 
 export function clientInfo(): string {
-  let version = require('../package.json').version
-  let name = require('../package.json').name
+  const version: string = require('../package.json').version
+  const name: string = require('../package.json').name
   return `${name}/${version}`
 }
 
