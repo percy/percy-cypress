c6ab4a6b586916384af17be810fcfcd3bb58381b
Ana Ulin <ana@ulin.org>
2019-03-27 14:08:01 -0700

    Make directory creation Node-8-compatible.

--- run-tests.js
+++ run-tests.js
@@ -4,6 +4,13 @@ const port = process.env.PORT_NUMBER || 8000;
 const spawn = require('child_process').spawn;
 const platform = require('os').platform();
 
+// Helper to make sure directories we need exist.
+function ensureDirExists(dir) {
+  if (! fs.existsSync(dir)) {
+    fs.mkdirSync(dir)
+  }
+}
+
 // Copy our healthcheck script to node_modules, where it will be
 // when this package is installed as a dependency.
 const src = 'lib/percy-healthcheck'
@@ -11,7 +18,10 @@ const copyDst = /^win/.test(platform)
       ? `${process.cwd()}\\node_modules\\@percy\\cypress\\dist\\percy-healthcheck`
       : `${__dirname}/node_modules/@percy/cypress/dist/percy-healthcheck`;
 console.log(`[run-tests] Copying ${src} to ${copyDst}`);
-fs.mkdirSync('./node_modules/@percy/cypress/dist', { recursive: true });
+// The 'recursive' fs.mkdirSync() option is only available in Node >10.12.0;
+// create all directories ourselves to accommodate Node 8.
+ensureDirExists('./node_modules/@percy/cypress')
+ensureDirExists('./node_modules/@percy/cypress/dist')
 fs.copyFileSync(src, copyDst);
 
 // We need to change the command path based on the platform they're using
