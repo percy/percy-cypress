15d48255d7345aa81ffda2ca51bf676b7a6cd3da
Robert DeLuca <robertdeluca19@gmail.com>
2019-08-02 14:14:36 -0500

    Move `task.js` to root for ease of require
    We don't need to transpile the task file since it's pure node. This can be
    required nicely in their projects with `require('@percy/cypress/task')` now.

--- lib/task.ts
+++
@@ -1,8 +0,0 @@
-import Axios from 'axios'
-
-export function percyHealthCheck() {
-  return Axios
-    .get('http://localhost:5338/percy/healthcheck')
-    .then(() =>  true)
-    .catch(() =>  false)
-}
