5d1fa0d3964ccb2ca8b2b6d590a3ba655dbd4697
Wil Wilsman <wil@wilwilsman.com>
2021-03-26 14:43:39 -0500

    ♻ Clean up

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,28 +1,7 @@
-// ***********************************************************
-// This example plugins/index.js can be used to load plugins
-//
-// You can change the location of this file or turn off loading
-// the plugins file with the 'pluginsFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/plugins-guide
-// ***********************************************************
-
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-const percyHealthCheck = require("../../task");
-
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+  // code coverage
+  require('@cypress/code-coverage/task')(on, config);
+  on('file:preprocessor', require('@cypress/code-coverage/use-babelrc'));
 
-  // Make it possible to log things to stdout by calling 'cy.task('log', 'some message to log').
-  // Useful for development and debugging.
-  on("task", {
-    log(message) {
-      console.log(message);
-      return null;
-    }
-  });
-  on("task", percyHealthCheck);
+  return config;
 };
