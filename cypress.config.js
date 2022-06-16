5d6ec47b92d50023aed638ea85b97a8da76a6613
Robert DeLuca <robertdeluca19@gmail.com>
2022-06-16 12:54:23 -0500

    ✂️ Remove unneeded comment in config file

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,6 @@ module.exports = defineConfig({
   video: false,
   screenshotOnRunFailure: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     }
