bd62f13971e83bb8fa95a5ba3246210b1b37e91d
Wil Wilsman <wil@wilwilsman.com>
2022-08-15 13:49:48 -0500

    ✅ Use Percy CLI testing mode for tests
    Updates tests to utilize CLI's new testing mode and updated `@percy/sdk-utils` test helpers.

--- cypress/e2e/index.cy.js
+++ cypress/e2e/index.cy.js
@@ -1,31 +1,22 @@
-import helpers from '@percy/sdk-utils/test/client';
+import helpers from '@percy/sdk-utils/test/helpers';
 
 const { match } = Cypress.sinon;
 
 describe('percySnapshot', () => {
   beforeEach(() => {
-    cy.then(helpers.setup);
-    cy.visit('http://localhost:8000');
+    cy.then(helpers.setupTest);
+    cy.visit(helpers.testSnapshotURL);
     cy.wrap(cy.spy(Cypress, 'log').log(false)).as('log');
   });
 
-  afterEach(() => {
-    cy.then(helpers.teardown);
-  });
-
   it('disables snapshots when the healthcheck fails', () => {
-    cy.then(() => helpers.testFailure('/percy/healthcheck'));
+    cy.then(() => helpers.test('error', '/percy/healthcheck'));
 
     cy.percySnapshot();
     cy.percySnapshot('Snapshot 2');
 
-    cy.then(helpers.getRequests).should('deep.equal', [
-      ['/percy/healthcheck']
-    ]);
-
-    cy.wrap(helpers.logger.stderr).should('deep.equal', []);
-    cy.wrap(helpers.logger.stdout).should('deep.equal', [
-      '[percy] Percy is not running, disabling snapshots'
+    cy.then(() => helpers.get('logs')).should('include.members', [
+      'Percy is not running, disabling snapshots'
     ]);
   });
 
@@ -33,36 +24,21 @@ describe('percySnapshot', () => {
     cy.percySnapshot();
     cy.percySnapshot('Snapshot 2');
 
-    cy.then(helpers.getRequests).should(requests => {
-      // test stub so we can utilize sinon matchers
-      let test = cy.stub(); test(requests);
-
-      expect(test).to.be.calledWith(match([
-        match(['/percy/healthcheck']),
-        match(['/percy/dom.js']),
-        match(['/percy/snapshot', match({
-          name: 'percySnapshot posts snapshots to the local percy server',
-          url: 'http://localhost:8000/',
-          domSnapshot: match(/<html><head>(.*?)<\/head><body>Snapshot Me<\/body><\/html>/),
-          clientInfo: match(/@percy\/cypress\/.+/),
-          environmentInfo: match(/cypress\/.+/)
-        })]),
-        match(['/percy/snapshot', match({
-          name: 'Snapshot 2'
-        })])
-      ]));
-    });
+    cy.then(() => helpers.get('logs'))
+      .should('include', `Snapshot found: ${cy.state('runnable').fullTitle()}`)
+      .should('include', `- url: ${helpers.testSnapshotURL}`)
+      .should('include.match', /clientInfo: @percy\/cypress\/.+/)
+      .should('include.match', /environmentInfo: cypress\/.+/)
+      .should('include', 'Snapshot found: Snapshot 2');
   });
 
   it('handles snapshot failures', () => {
-    cy.then(() => helpers.testFailure('/percy/snapshot'));
+    cy.then(() => helpers.test('error', '/percy/snapshot'));
 
     cy.percySnapshot();
 
-    cy.wrap(helpers.logger.stdout).should('deep.equal', []);
-    cy.wrap(helpers.logger.stderr).should('deep.equal', [
-      '[percy] Could not take DOM snapshot "percySnapshot handles snapshot failures"',
-      '[percy] Error: 500 Internal Server Error'
+    cy.then(() => helpers.get('logs')).should('include.members', [
+      'Could not take DOM snapshot "percySnapshot handles snapshot failures"'
     ]);
   });
 
