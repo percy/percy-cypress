efed8c5ccae92630170c2d86e6580c2ac588b4e7
Ethan Setnik <esetnik@gmail.com>
2022-11-02 12:14:36 -0400

    Rethrow the error to cypress
    When percy fails to take the snapshot we should rethrow the error to cypress to fail the test case.
    
    Resolves #568

--- index.js
+++ index.js
@@ -73,6 +73,7 @@ Cypress.Commands.add('percySnapshot', (name, options) => {
         // Handle errors
         log.error(`Could not take DOM snapshot "${name}"`);
         log.error(error);
+        throw error;
       });
     });
   });
