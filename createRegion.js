eefb9e43df0a302fd6a8267202db047e7eaa6151
Amandeep singh <amandeep@browserstack.com>
2025-04-04 18:35:31 +0530

    aded regions support (#929)
    * aded regions
    
    * lint
    
    * test
    
    * updated cli to 1.30.9
    
    * resolved comments

---
+++ createRegion.js
@@ -0,0 +1,53 @@
+const createRegion = function({
+  boundingBox = null,
+  elementXpath = null,
+  elementCSS = null,
+  padding = null,
+  algorithm = 'ignore',
+  diffSensitivity = null,
+  imageIgnoreThreshold = null,
+  carouselsEnabled = null,
+  bannersEnabled = null,
+  adsEnabled = null,
+  diffIgnoreThreshold = null
+} = {}) {
+  const elementSelector = {};
+  if (boundingBox) elementSelector.boundingBox = boundingBox;
+  if (elementXpath) elementSelector.elementXpath = elementXpath;
+  if (elementCSS) elementSelector.elementCSS = elementCSS;
+
+  const region = {
+    algorithm,
+    elementSelector
+  };
+
+  if (padding) {
+    region.padding = padding;
+  }
+
+  const configuration = {};
+  if (['standard', 'intelliignore'].includes(algorithm)) {
+    if (diffSensitivity) configuration.diffSensitivity = diffSensitivity;
+    if (imageIgnoreThreshold) configuration.imageIgnoreThreshold = imageIgnoreThreshold;
+    if (carouselsEnabled) configuration.carouselsEnabled = carouselsEnabled;
+    if (bannersEnabled) configuration.bannersEnabled = bannersEnabled;
+    if (adsEnabled) configuration.adsEnabled = adsEnabled;
+  }
+
+  if (Object.keys(configuration).length > 0) {
+    region.configuration = configuration;
+  }
+
+  const assertion = {};
+  if (diffIgnoreThreshold) {
+    assertion.diffIgnoreThreshold = diffIgnoreThreshold;
+  }
+
+  if (Object.keys(assertion).length > 0) {
+    region.assertion = assertion;
+  }
+
+  return region;
+};
+
+module.exports = { createRegion };
